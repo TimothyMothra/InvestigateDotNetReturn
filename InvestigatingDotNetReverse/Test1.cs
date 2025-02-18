166c1780eece885f9e8c9c74b12c0c0ad5395639
Timothy Mothra Lee (from Dev Box) <tilee@microsoft.com>
2025-02-18 13:29:56 -0800

    checkin test project

---
+++ InvestigatingDotNetReverse/Test1.cs
@@ -0,0 +1,24 @@
+﻿namespace InvestigatingDotNetReverse
+{
+    using System.Collections.Concurrent;
+    //using System.Linq;
+
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var stack = new ConcurrentStack<string>(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]);
+
+            // Act
+            var test = stack.ToArray().Reverse().ToList();
+
+            // Assert
+            Assert.Equals(10, test.Count);
+            //Assert.Contains("one", array);
+            //Assert.Contains("ten", array);
+        }
+    }
+}
